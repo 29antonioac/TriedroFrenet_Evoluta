12cd8ded017266fdd44e906231504727a84a35dc
analca3 <analca3@gmail.com>
2015-06-03 15:23:23 +0200

    Añadida comprobación de botón cancelar

--- GUI.py
+++ GUI.py
@@ -8,20 +8,29 @@ if __name__ == "__main__":
     msgbox("Hola! Este programa dibuja una curva en R^3 junto a su Triedro de Frenet y su evoluta.")
 
     while 1:
-        
+
         correcto = False
 
         while not correcto:
-            
+
             x_t = enterbox("Introduce la función x(t)", "Función x(t)")
+            if x_t is None: sys.exit(-1)
+
             y_t = enterbox("Introduce la función y(t)", "Función y(t)")
+            if y_t is None: sys.exit(-1)
+
             z_t = enterbox("Introduce la función z(t)", "Función z(t)")
+            if z_t is None: sys.exit(-1)
 
             num_puntos = enterbox("Introduce el número de muestras", "Número de muestras")
+            if num_puntos is None: sys.exit(-1)
 
             inicio = enterbox("Introduce el inicio del intervalo", "Inicio del intervalo")
-            final = enterbox("Introduce final del intervalo", "Final del intervalo")
+            if inicio is None: sys.exit(-1)
             
+            final = enterbox("Introduce final del intervalo", "Final del intervalo")
+            if final is None: sys.exit(-1)
+
             if x_t is "" or y_t is "" or z_t is "" or num_puntos is "" or inicio is "" or final is "":
                 correcto = False
                 msgbox("Te has dejado algún dato sin introducir")
